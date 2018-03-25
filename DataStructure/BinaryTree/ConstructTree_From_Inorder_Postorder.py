d59db8da45eb5700858f325d31e9509347a3a257
Hu Yao <hooyao@gmail.com>
2018-03-25 16:40:51 +0800

    add lousy slow solution for tree building

---
+++ DataStructure/BinaryTree/ConstructTree_From_Inorder_Postorder.py
@@ -0,0 +1,69 @@
+import sys
+
+from BTreeUtils import BTreeHelper
+from BTreeUtils import TreeNode
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        return self.recursive_build_from_inorder_postorder(inorder, postorder)
+
+    def recursive_build_from_inorder_postorder(self, in_order, post_order):
+        if len(in_order) != len(post_order):
+            raise ValueError(
+                'inorder len = {0} must be equal to postorder len= {1}'.format(len(in_order), len(post_order)))
+        if len(in_order) == 0:
+            return None
+        root_val = post_order[-1]
+        root_node = TreeNode(root_val)
+        in_order_root_index = in_order.index(root_val)
+        in_order_left_part = in_order[:in_order_root_index]
+        in_order_right_part = in_order[in_order_root_index + 1:]
+
+        post_order_left_end = 0
+        if len(in_order_left_part) == 0:
+            post_order_left_part = []
+        else:
+            for idx in reversed(range(0, len(in_order) - 1)):
+                if set(post_order[0:idx+1]) == set(in_order_left_part):
+                    post_order_left_end = idx
+                    break
+            post_order_left_part = post_order[0:post_order_left_end+1]
+        post_order_right_part_first_index = len(post_order_left_part)
+        post_order_right_part = post_order[post_order_right_part_first_index:-1]
+
+        left_root = self.recursive_build_from_inorder_postorder(in_order_left_part, post_order_left_part)
+        righ_root = self.recursive_build_from_inorder_postorder(in_order_right_part, post_order_right_part)
+
+        root_node.left = left_root
+        root_node.right = righ_root
+        return root_node
+
+
+def main(*args):
+    inorder = [9, 3, 15, 20, 7]
+    postorder = [9, 15, 7, 20, 3]
+    #inorder = [1, 2, 3]
+    #postorder = [3, 2, 1]
+    #inorder = [2, 1]
+    #postorder = [2, 1]
+    result = Solution().buildTree(inorder, postorder)
+    BTreeHelper.pretty_print(result)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
---
+++ DataStructure/BinaryTree/ConstructTree_From_Inorder_Preorder.py
@@ -0,0 +1,65 @@
+import sys
+
+from BTreeUtils import BTreeHelper
+from BTreeUtils import TreeNode
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        return self.recursive_build_from_inorder_postorder(preorder, inorder)
+
+    def recursive_build_from_inorder_postorder(self, pre_order, in_order):
+        if len(in_order) != len(pre_order):
+            raise ValueError(
+                'inorder len = {0} must be equal to preorder len= {1}'.format(len(in_order), len(pre_order)))
+        if len(in_order) == 0:
+            return None
+        root_val = pre_order[0]
+        root_node = TreeNode(root_val)
+        in_order_root_index = in_order.index(root_val)
+        in_order_left_part = in_order[:in_order_root_index]
+        in_order_right_part = in_order[in_order_root_index + 1:]
+
+        pre_order_left_end = 0
+        if len(in_order_left_part) == 0:
+            pre_order_left_part = []
+        else:
+            for idx in reversed(range(1, len(in_order))):
+                if set(pre_order[1:idx + 1]) == set(in_order_left_part):
+                    pre_order_left_end = idx
+                    break
+            pre_order_left_part = pre_order[1:pre_order_left_end + 1]
+        pre_order_right_part_first_index = len(pre_order_left_part) + 1
+        pre_order_right_part = pre_order[pre_order_right_part_first_index:]
+
+        left_root = self.recursive_build_from_inorder_postorder(pre_order_left_part, in_order_left_part)
+        righ_root = self.recursive_build_from_inorder_postorder(pre_order_right_part, in_order_right_part)
+
+        root_node.left = left_root
+        root_node.right = righ_root
+        return root_node
+
+
+def main(*args):
+    preorder = [3, 9, 20, 15, 7]
+    inorder = [9, 3, 15, 20, 7]
+    result = Solution().buildTree(preorder, inorder)
+    BTreeHelper.pretty_print(result)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
