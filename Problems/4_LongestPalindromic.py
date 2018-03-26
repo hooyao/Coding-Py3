51dbcf3c6f17b634d471960339ece9a1f76bbd9a
Hu Yao <hooyao@gmail.com>
2018-03-26 23:09:54 +0800

    init longest palindromic

---
+++ Problems/4_LongestPalindromic.py
@@ -0,0 +1,19 @@
+import sys
+
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+def main(*args):
+    s = "babad"
+    s1 = "cbbd"
+    solution = Solution()
+    result = solution.longestPalindrome(s)
+    print(result)
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
